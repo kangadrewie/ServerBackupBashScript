347e67f2ea1ea2c4efaaa5d60e300e8dd1ab32f3
root <root@ip-172-26-4-173.eu-west-1.compute.internal>
2020-03-29 22:24:54 +0000

    timer and push changes

--- backup.sh
+++ backup.sh
@@ -1,15 +1,30 @@
 #! /bin/bash
 
-BACKUP_PATH=/var/www/html/Intranet
+BACKUP_PATH=/var/www/html/Live
 
-#Change permissions
+#Change permissions for Live Folder
 sudo chmod 000 $BACKUP_PATH
 
 #Zip the live folder with the current time and date to backup folder
 dt=`date '+%d-%m-%Y_%H:%M:%S'`
 zip -r /home/ubuntu/backups/$dt.zip $BACKUP_PATH
 
-#Change Permissions back
-sudo chmod 744 $BACKUP_PATH
+#Add Date to Backup History Log
+date >> /home/ubuntu/BackupLog.txt
+echo "" >> /home/ubuntu/BackupLog.txt
 
-echo "Successful Backup"
+#Change Permissions for Intranet Folder
+sudo chmod 000 /var/www/html/Intranet
+
+#Copy new or modified files to live folder
+sudo cp -u -r /var/www/html/Intranet/. $BACKUP_PATH
+
+#Change permissions back to Intranet Folder to read and write
+sudo chmod 744 /var/www/html/Intranet
+#Change Permission back to Live Folder to read only
+sudo chmod 0444 $BACKUP_PATH
+
+
+#Generate Health Report
+sudo chmod 744 /home/ubuntu/HealthReports/
+sudo vmstat -t 3 5 > /home/ubuntu/HealthReports/$dt.txt 
--- script.sh
+++ script.sh
@@ -20,30 +20,23 @@ backup() {
 		 )	
 
 	CHOICE=$(dialog --clear \
-				--cancel-label "Go Back" \
+				--nocancel \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
                 --menu "$MENU" \
                 $HEIGHT $WIDTH $CHOICE_HEIGHT \
                 "${OPTIONS[@]}" \
                 2>&1 > /dev/tty)
-
-	EXIT=$?
-
-	case $EXIT in
-		$DIALOG_CANCEL)
-			gui
-			;;
-	esac 
+ 
 
 	case $CHOICE in
-		
 		#Change directory location of Backup
 		1)
 			INPUT=$(\dialog --title "Backup Directory" \
-		          			    --inputbox "Enter the directory name:" 8 40 \
-							   				    3>&1 1>&2 2>&3 3>&- \
-											 			)	
+							--nocancel \
+		          			--inputbox "Enter the directory name:" 8 40 \
+							3>&1 1>&2 2>&3 3>&-)
+
 			NON_ESCAPED_INPUT=$INPUT
 			INPUT=$(sed 's|/|\\/|g' <<< $INPUT)
 			sed -i "s/^BACKUP_PATH.*/BACKUP_PATH=${INPUT}/" /home/ubuntu/backup.sh
@@ -51,24 +44,41 @@ backup() {
 			dialog --msgbox "Backup Directory has been changed to ${NON_ESCAPED_INPUT}. This will take affect when backup runs again." 15 40
 			gui
 			;;
-		
+			
 		#Check Backup status
 		2)
-			
+			dialog --msgbox "Backup is RUNNING for the following:\n\n$(sudo crontab -l)" 8 50
+			gui
 			;;
 
 		#Change time of backup
 		3)
+			TIMER_INPUT=$(\dialog --title "Backup Timer" \
+							--nocancel \
+		          			--timebox "Enter time of day for Backup:" 5 35 15 30 00 \
+							3>&1 1>&2 2>&3 3>&-)
+
+			HOUR="$(cut -d':' -f1 <<<$TIMER_INPUT)"
+			MINUTES="$(cut -d':' -f2 <<<$TIMER_INPUT)"
 			
+			
+		 	sudo crontab -l > backupInterval
+			sudo echo "${MINUTES} ${HOUR} * * * /home/ubuntu/backup.sh" > backupInterval
+			
+			sudo crontab backupInterval
+			sudo rm backupInterval
+
+			sudo dialog --msgbox "A daily backup has been set for ${HOUR}: ${MINUTES}." 15 45
+			gui
 			;;
 
 		4)
+			#History of Backups
 			dialog --textbox /home/ubuntu/BackupLog.txt 50 80
 			gui
 			;;
  
 	esac
-
 }
 
 #Log changes made to directory
@@ -82,31 +92,25 @@ auditLog() {
          	 3 "Search User Audit")
 
 	CHOICE=$(dialog --clear \
-				--cancel-label "Go Back" \
+				--nocancel \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
                 --menu "$MENU" \
                 $HEIGHT $WIDTH $CHOICE_HEIGHT \
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
-				
-				EXIT=$?
-				
-				case $EXIT in
-					$DIALOG_CANCEL)
-						gui
-						;;
-				esac
-
+			
 	case $CHOICE in
 		1)
 			#Generate Full Audit Report to text file
 			sudo ausearch -f /var/www/html/Intranet/ > /home/ubuntu/AuditLog.txt
 			dialog --textbox /home/ubuntu/AuditLog.txt 50 80
-
+			
+			
 			dialog --msgbox "A .txt report is available and can be viewed on /home/ubuntu/AuditLog.txt" 8 40 
 			gui
 			;;
+			
 		2)
 			#Generate Summary Report
 			sudo ausearch -f /var/www/html/Intranet | aureport -f -i > /home/ubuntu/SummaryReport.txt
@@ -116,25 +120,56 @@ auditLog() {
 		3)
 			#Search Users Audit Report
 			INPUT=$(\dialog --title "Search User" \
-         			    --inputbox "Enter Username:" 8 40 \
-  				    3>&1 1>&2 2>&3 3>&- \
+         			        --cancel-label "Go Back" \
+							--inputbox "Enter Username:" 8 40 \
+  				            3>&1 1>&2 2>&3 3>&- \
 			)
 
+			
 			mkdir "$INPUT"
 			sudo ausearch -f /var/www/html/Intranet/ | aureport -f -i | grep $INPUT > /home/ubuntu/UserReport.txt
 			dialog --cr-wrap \
-			       --textbox /home/ubuntu/UserReport.txt 50 80
-			gui	
-			;;
+			      	--textbox /home/ubuntu/UserReport.txt 50 80
+			gui		
+				
 	esac 
 }
 
 #Push any new changes to live site
-#pushChanges() {
+pushChanges() {
 
+	dialog 	--backtitle "Manually Push Changes" \
+			--yesno "Are you sure you want to push changes to /var/www/html/Live?" 7 60
 
+	RESPONSE=$?
+	case $RESPONSE in
+		0)
+			#Change Permissions
+			sudo chmod 000 /var/www/html/Intranet
+			sudo chmod 000 /var/www/html/Live
+ 			
+			#Only copy what has been newly created or modified
+			sudo cp -u -r -v /var/www/html/Intranet/. /var/www/html/Live > /home/ubuntu/Transfers.txt 
+			
+			#Change Permission back
+			sudo chmod 733 /var/www/html/Intranet
+			sudo chmod 0444  /var/www/html/Live
+			
+			#Display what has been pushed to Live Folder
+			dialog --textbox /home/ubuntu/Transfers.txt 50 80 
+			gui	
+			;;
+		1)
+			gui
+			;;
 
-#}
+		255)
+			gui
+			;;
+			 			
+	esac
+
+}
 
 #Check Apache2 Status
 apacheStatus() {
@@ -203,6 +238,9 @@ gui() {
 			;;
 		5)
 			systemHealth
+			;;
+		6)
+			pushChanges
 			;;
 	esac
  
